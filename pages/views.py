cece50643bb56f6f812727f14ee773eb17fa81a5
desokan <thefdmm@gmail.com>
2024-03-23 06:14:51 +0000

    formatted with html tag

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homePageView(request):
-    return HttpResponse('Salaam Alaykum!')
+    return HttpResponse('<h1>Salaam Alaykum!</h1>')
 
